5c2cf9ced208512d16261b3f91dcc30df683594a
Tijs Alleman <tijs.alleman@ugent.be>
2022-11-08 12:59:48 +0100

    Copy relevant files from covid19model

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='pySODM',
+    packages=find_packages("src", exclude=["*.tests"]),
+    package_dir={'': 'src'},
+    version='0.1',
+    description='COVID-19 modelling package',
+    author='Tijs Alleman, BIOMATH, Ghent University',
+    license='MIT',
+    install_requires=[
+        'matplotlib',
+        'numpy',
+        'scipy',
+        'pandas',
+        'xlrd',
+	    'openpyxl',
+        'zarr',
+        'emcee',
+        'xarray',
+        'rbfopt',
+        'numba',
+        'SAlib',
+        'h5py'
+    ],
+    extras_require={
+        "develop":  ["pytest",
+                     "sphinx",
+                     "numpydoc",
+                     "sphinx_rtd_theme",
+                     "myst_parser[sphinx]"],
+    }
+)
