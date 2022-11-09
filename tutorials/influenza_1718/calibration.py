94077e6ea030064bed225c6630f04b7d87245cd9
Tijs Alleman <tijs.alleman@ugent.be>
2022-11-09 15:28:07 +0100

    influenza calibration script

--- tutorials/influenza_1718/calibration.py
+++ tutorials/influenza_1718/calibration.py
@@ -23,9 +23,9 @@ import matplotlib.pyplot as plt
 from pySODM.models.base import BaseModel #from covid19model.models.base import BaseModel
 from pySODM.optimization import pso, nelder_mead
 from pySODM.optimization.mcmc import perturbate_theta, run_EnsembleSampler, emcee_sampler_to_dictionary
-from pySODM.optimization.objective_functions import log_posterior_probability, log_prior_uniform, ll_poisson, ll_negative_binomial
-# COVID-19 model
-from covid19model.data.utils import construct_initN
+from pySODM.optimization.objective_functions import log_posterior_probability, log_prior_uniform, ll_poisson
+# pySODM dependecies
+import corner
 
 # Suppress warnings
 import warnings
@@ -36,7 +36,7 @@ warnings.filterwarnings("ignore")
 ###############
 
 # Load data
-data = pd.read_csv(os.path.join(os.path.dirname(__file__),'../../data/interim/influenza/dataset_influenza_1718_format.csv'), index_col=[0,1], parse_dates=True)
+data = pd.read_csv(os.path.join(os.path.dirname(__file__),'data/interim/data_influenza_1718_format.csv'), index_col=[0,1], parse_dates=True)
 data = data.squeeze()
 # Re-insert pd.IntervalIndex (pd.IntervalIndex is always loaded as a string..)
 age_groups = pd.IntervalIndex.from_tuples([(0,5),(5,15),(15,65),(65,120)])
@@ -44,6 +44,8 @@ iterables = [data.index.get_level_values('DATE').unique(), age_groups]
 names = ['date', 'Nc']
 index = pd.MultiIndex.from_product(iterables, names=names)
 df_influenza = pd.Series(index=index, name='CASES', data=data.values)
+# Hardcode Belgian demographics
+initN = pd.Series(index=age_groups, data=[606938, 1328733, 7352492, 2204478])
 
 ##################
 ## Define model ##
@@ -51,28 +53,28 @@ df_influenza = pd.Series(index=index, name='CASES', data=data.values)
 
 class influenza_model(BaseModel):
     """
-    Simple model for influenza
+    Simple SEIR model for influenza with undetected carriers
     """
     
-    state_names = ['S','E','Ia','Im','Is','R','D']
-    parameter_names = ['beta','sigma','f_a','f_m','gamma_m','gamma_s']
-    parameters_stratified_names = [['mu'],]
+    state_names = ['S','E','Ia','Im','R','Im_new']
+    parameter_names = ['beta','sigma','f_a','gamma']
     stratification = ['Nc']
-    coordinates = [None]
-    
+
     @staticmethod
-    def integrate(t, S, E, Ia, Im, Is, R, D, beta, sigma, f_a, f_m, gamma_m, gamma_s, mu, Nc):
+    def integrate(t, S, E, Ia, Im, R, Im_new, beta, sigma, f_a, gamma, Nc):
         
-        T = S+E+Ia+Im+Is+R
+        # Calculate total population
+        T = S+E+Ia+Im+R
+        # Calculate differentials
         dS = -beta*Nc@((Ia+Im)*S/T)
         dE = beta*Nc@((Ia+Im)*S/T) - 1/sigma*E
-        dIa = f_a*E/sigma - 1/gamma_m*Ia
-        dIm = (1-f_a)/sigma*E-1/gamma_m*Im
-        dIs = (1-f_m)/gamma_m*Im-(1/gamma_s)*Is-mu*Is
-        dR = f_m/gamma_m*Im+(1/gamma_s)*Is+1/gamma_m*Ia
-        dD = mu*Is
-        
-        return dS, dE, dIa, dIm, dIs, dR, dD   
+        dIa = f_a*E/sigma - 1/gamma*Ia
+        dIm = (1-f_a)/sigma*E - 1/gamma*Im
+        dR = 1/gamma*(Ia+Im)
+        # Calculate incidence mild disease
+        dIm_new = (1-f_a)/sigma*E - Im_new
+
+        return dS, dE, dIa, dIm, dR, dIm_new
 
 #################
 ## Setup model ##
@@ -91,11 +93,12 @@ Nc = np.array([[1.3648649, 1.1621622, 5.459459, 0.3918919],
              [0.5524476, 5.1328671,  6.265734, 0.4055944],
              [0.3842975, 0.8409091, 10.520661, 0.9008264],
              [0.2040816, 0.5918367,  4.612245, 2.1428571]])
-# Define model parameters, initial states and coordinates
-params={'beta':0.10,'sigma':1,'f_a':0,'f_m':0.5,'gamma_m':1,'gamma_s':8.6,'mu':[0.01, 0.05, 0.08, 0.13],'Nc':np.transpose(Nc)}
-init_states = {'S':construct_initN(age_groups).values,'E':construct_initN(age_groups).values/construct_initN(age_groups).values[0]}
+# Define model parameters
+params={'beta':0.10,'sigma':1,'f_a':0.75,'gamma':5,'Nc':np.transpose(Nc)}
+# Define initial condition
+init_states = {'S': initN.values,'E': initN.values/initN.values[0]}
+# Define model coordinates
 coordinates=[age_groups,]
-
 # Initialize model
 model = influenza_model(init_states,params,coordinates)
 
@@ -112,7 +115,7 @@ if __name__ == '__main__':
     # Maximum number of PSO iterations
     n_pso = 10
     # Maximum number of MCMC iterations
-    n_mcmc = 30
+    n_mcmc = 100
     # PSO settings
     processes = int(os.getenv('SLURM_CPUS_ON_NODE', mp.cpu_count()/2))
     multiplier_pso = 30
@@ -124,18 +127,16 @@ if __name__ == '__main__':
     print_n = 5
     # Define dataset
     data=[df_influenza[start_date:end_date], ]
-    states = ["Im",]
+    states = ["Im_new",]
     weights = np.array([1,]) # Scores of individual contributions: Dataset: 0, total ll: -4590, Dataset: 1, total ll: -4694, Dataset: 2, total ll: -4984
     log_likelihood_fnc = [ll_poisson,]
     log_likelihood_fnc_args = [[],]
-
     # Calibated parameters and bounds
     pars = ['beta', 'f_a']
-    bounds = [(0.05,0.15), (0,1)]
+    bounds = [(0.001,0.15), (0,1)]
     # Setup prior functions and arguments
     log_prior_fnc = len(bounds)*[log_prior_uniform,]
     log_prior_fnc_args = bounds
-
     # Setup objective function without priors and with negative weights 
     objective_function = log_posterior_probability([],[],model,pars,data,states,
                                                log_likelihood_fnc,log_likelihood_fnc_args,-weights)
@@ -143,7 +144,7 @@ if __name__ == '__main__':
     #theta = pso.optimize(objective_function, bounds, kwargs={'simulation_kwargs':{'warmup': warmup}},
     #                   swarmsize=multiplier_pso*processes, maxiter=n_pso, processes=processes, debug=True)[0]
     # A good guess
-    theta = [0.10, 0.10]         
+    theta = [0.030, 0.90]         
     # Nelder-mead
     step = len(bounds)*[0.10,]
     theta = nelder_mead.optimize(objective_function, np.array(theta), bounds, step, kwargs={'simulation_kwargs':{'warmup': warmup}}, processes=processes, max_iter=n_pso)[0]
@@ -156,15 +157,19 @@ if __name__ == '__main__':
     model.parameters.update({'beta': theta[0],})
     out = model.sim(end_date, start_date=start_date, warmup=warmup)
 
+    # Visualize
     fig, axs = plt.subplots(2,2,sharex=True, sharey=True, figsize=(8,6))
     axs = axs.reshape(-1)
     for id, age_class in enumerate(df_influenza.index.get_level_values('Nc').unique()):
-        axs[id].plot(out['time'],out.sel(Nc=age_class)['Im'], color='black')
+        axs[id].plot(out['time'],out.sel(Nc=age_class)['Im_new'], color='black')
         axs[id].plot(df_influenza.index.get_level_values('date').unique(),df_influenza.loc[slice(None),age_class], color='orange')
         axs[id].set_title(age_class)
-        axs[id].legend(['$I_{m}$','data'])
-        axs[id].xaxis.set_major_locator(plt.MaxNLocator(2))
+        axs[id].legend(['$I_{m, new}$','data'])
+        axs[id].xaxis.set_major_locator(plt.MaxNLocator(5))
+        for tick in axs[id].get_xticklabels():
+            tick.set_rotation(30)
         axs[id].grid(False)
+    plt.tight_layout()
     plt.show()
     plt.close()
 
@@ -173,28 +178,25 @@ if __name__ == '__main__':
     ##########
 
     # Perturbate previously obtained estimate
-    ndim, nwalkers, pos = perturbate_theta(theta, pert=[0.10, 0.01], multiplier=multiplier_mcmc, bounds=log_prior_fnc_args, verbose=True)
+    ndim, nwalkers, pos = perturbate_theta(theta, pert=[0.02, 0.02], multiplier=multiplier_mcmc, bounds=log_prior_fnc_args)
     # Labels for traceplots
     labels = ['$\\beta$', '$f_a$']
     pars_postprocessing = ['beta', 'f_a']
     # Variables
-    samples_path=None
-    fig_path=None
-    identifier = 'woldemun_test'
+    samples_path='sampler_output/'
+    fig_path='sampler_output/'
+    identifier = 'username'
     run_date = str(datetime.date.today())
     # initialize objective function
     objective_function = log_posterior_probability(log_prior_fnc,log_prior_fnc_args,model,pars,data,states,log_likelihood_fnc,log_likelihood_fnc_args,weights)
     # Write some usefull settings to a pickle file (no pd.Timestamps or np.arrays allowed!)
     settings={'start_calibration': start_date.strftime("%Y-%m-%d"), 'end_calibration': end_date.strftime("%Y-%m-%d"), 'n_chains': nwalkers,
                 'warmup': warmup, 'labels': labels, 'parameters': pars_postprocessing, 'starting_estimate': list(theta)}
-    # Start calibration
-    print(f'Using {processes} cores for {ndim} parameters, in {nwalkers} chains.\n')
-    sys.stdout.flush()
     # Sample 100 iterations
     sampler = run_EnsembleSampler(pos, max_n, identifier, objective_function, (), {'simulation_kwargs': {'warmup': warmup}},
                                     fig_path=fig_path, samples_path=samples_path, print_n=print_n, labels=labels, backend=None, processes=processes, progress=True,
                                     settings_dict=settings)
-    backend = emcee.backends.HDFBackend('woldemun_test_BACKEND_'+run_date+'.h5')
+    backend = emcee.backends.HDFBackend(os.path.join(os.getcwd(),samples_path+'username_BACKEND_'+run_date+'.h5'))
     # Sample 100 more
     sampler = run_EnsembleSampler(pos, max_n, identifier, objective_function, (), {'simulation_kwargs': {'warmup': warmup}},
                                     fig_path=fig_path, samples_path=samples_path, print_n=print_n, labels=labels, backend=backend, processes=processes, progress=True,
@@ -206,7 +208,6 @@ if __name__ == '__main__':
     with open(str(identifier)+'_SAMPLES_'+run_date+'.json', 'w') as fp:
         json.dump(samples_dict, fp)
     # Look at the resulting distributions in a cornerplot
-    import corner
     CORNER_KWARGS = dict(smooth=0.90,title_fmt=".2E")
     fig = corner.corner(sampler.get_chain(discard=50, thin=2, flat=True), labels=labels, **CORNER_KWARGS)
     for idx,ax in enumerate(fig.get_axes()):
@@ -218,28 +219,29 @@ if __name__ == '__main__':
     ## Visualize result ##
     ######################
 
+    # TODO: Observational noise!
+
     # Define draw function
     def draw_fcn(param_dict, samples_dict):
-        # @Wolf: Waarom zorg ik ervoor dat de samples voor 'beta' en 'f_a' van dezelfde positie 'idx' komen?
         idx, param_dict['beta'] = random.choice(list(enumerate(samples_dict['beta'])))  
         param_dict['f_a'] = samples_dict['f_a'][idx]
         return param_dict
-    
     # Simulate model
-    out = model.sim(end_date, start_date=start_date, warmup=warmup, N=50, samples=samples_dict, draw_fcn=draw_fcn, processes=processes)
-
-    # Make visualization
+    out = model.sim(end_date, start_date=start_date, warmup=warmup, N=20, samples=samples_dict, draw_fcn=draw_fcn, processes=processes)
+    # Visualize
     fig, axs = plt.subplots(2,2,sharex=True, sharey=True, figsize=(8,6))
     axs = axs.reshape(-1)
     for id, age_class in enumerate(df_influenza.index.get_level_values('Nc').unique()):
-        axs[id].plot(out['time'].values,out['Im'].sel(Nc=age_class).mean(dim='draws'), color='black') # Dimensie erbij gekomen: draws !
-        axs[id].fill_between(out['time'].values,out['Im'].sel(Nc=age_class).quantile(dim='draws', q=0.025),
-                             out['Im'].sel(Nc=age_class).quantile(dim='draws', q=0.975), color='black', alpha=0.1)
+        axs[id].plot(out['time'],out['Im_new'].sel(Nc=age_class).mean(dim='draws'), color='black')
+        axs[id].fill_between(out['time'].values,out['Im_new'].sel(Nc=age_class).quantile(dim='draws', q=0.025),
+                             out['Im_new'].sel(Nc=age_class).quantile(dim='draws', q=0.975), color='black', alpha=0.1)
         axs[id].plot(df_influenza.index.get_level_values('date').unique(),df_influenza.loc[slice(None),age_class], color='orange')
         axs[id].set_title(age_class)
-        axs[id].legend(['$I_{m}$','confint', 'data'])
-        axs[id].xaxis.set_major_locator(plt.MaxNLocator(2))
+        axs[id].legend(['$I_{m, new}$','data'])
+        axs[id].xaxis.set_major_locator(plt.MaxNLocator(5))
+        for tick in axs[id].get_xticklabels():
+            tick.set_rotation(30)
         axs[id].grid(False)
+    plt.tight_layout()
     plt.show()
     plt.close()
-    
