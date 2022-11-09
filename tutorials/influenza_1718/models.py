67b5f918b65a4f4dd52326a6f8419fa4b8d191f9
Tijs Alleman <tijs.alleman@ugent.be>
2022-11-09 18:32:53 +0100

    add stochstic model

--- tutorials/influenza_1718/models.py
+++ tutorials/influenza_1718/models.py
@@ -1,16 +1,17 @@
+import numpy as np
 from pySODM.models.base import BaseModel
 
-class influenza_model(BaseModel):
+class ODE_influenza_model(BaseModel):
     """
     Simple SEIR model for influenza with undetected carriers
     """
     
-    state_names = ['S','E','Ia','Im','R','Im_new']
+    state_names = ['S','E','Ia','Im','R','Im_inc']
     parameter_names = ['beta','sigma','f_a','gamma']
     stratification = ['Nc']
 
     @staticmethod
-    def integrate(t, S, E, Ia, Im, R, Im_new, beta, sigma, f_a, gamma, Nc):
+    def integrate(t, S, E, Ia, Im, R, Im_inc, beta, sigma, f_a, gamma, Nc):
         
         # Calculate total population
         T = S+E+Ia+Im+R
@@ -21,6 +22,41 @@ class influenza_model(BaseModel):
         dIm = (1-f_a)/sigma*E - 1/gamma*Im
         dR = 1/gamma*(Ia+Im)
         # Calculate incidence mild disease
-        dIm_new = (1-f_a)/sigma*E - Im_new
+        dIm_inc_new = (1-f_a)/sigma*E - Im_inc
 
-        return dS, dE, dIa, dIm, dR, dIm_new
+        return dS, dE, dIa, dIm, dR, dIm_inc_new
+
+class SDE_influenza_model(BaseModel):
+    """
+    Simple stochastic SEIR model for influenza with undetected carriers
+    """
+    
+    state_names = ['S','E','Ia','Im','R','Im_inc']
+    parameter_names = ['beta','sigma','f_a','gamma']
+    stratification = ['Nc']
+
+    @staticmethod
+    def integrate(t, l, S, E, Ia, Im, R, Im_inc, beta, sigma, f_a, gamma, Nc):
+        
+        # Calculate total population
+        T = S+E+Ia+Im+R
+
+        # Define the rates of the transitionings
+        states = [S, E, Ia, Im]
+        rates = [beta*np.matmul(Nc, (Ia+Im)/T), 1/sigma, 1/gamma, 1/gamma]
+
+        # Draw the number of transitionings
+        N=[]
+        for i, rate in enumerate(rates):
+            N.append(np.random.binomial(states[i].astype('int64'), 1-np.exp(-l*rate), size=len(S))) 
+
+        # Update the system
+        S_new  = S - N[0]
+        E_new = E + N[0] - np.rint(f_a*N[1]) - np.rint((1-f_a)*N[1])
+        Ia_new = Ia + np.rint(f_a*N[1]) - N[2]
+        Im_new = Im + np.rint((1-f_a)*N[1]) - N[3]
+        R_new = R + N[2] + N[3]
+        # Calculate incidence mild disease
+        Im_inc_new = np.rint((1-f_a)*N[1])/l
+
+        return S_new, E_new, Ia_new, Im_new, R_new, Im_inc_new
