142c3e795b1ce0ff19a1b7dd8d19913486abbf98
Tijs Alleman <tijs.alleman@ugent.be>
2022-12-09 12:51:19 +0100

    1D PFR model

---
+++ tutorials/enzyme_kinetics/1D_PFR/models.py
@@ -0,0 +1,72 @@
+import numpy as np
+from pySODM.models.base import ODEModel
+
+class packed_PFR(ODEModel):
+    """
+    A model of a packed-bed plug-flow reactor with axial dispersion in one dimension
+    At the surface of the catalyst, the enzymatic esterification conversion of D-Glucose and Lauric acid into Glucose Laurate Ester and water takes place
+    S + A <--> Es + W
+    """
+
+    state_names = ['S', 'A', 'Es', 'W']
+    parameter_names = ['epsilon', 'kL_a', 'D_ax', 'delta_x', 'u', 'rho_B', 'inlet']
+    stratification_names = ['phase', 'x']
+
+    @staticmethod
+    def integrate(t, S, A, Es, W, epsilon, kL_a, D_ax, delta_x, u, rho_B, inlet):
+
+        # Initialize derivatives
+        dS = np.zeros(S.shape, dtype=np.float64)
+        dA = np.zeros(A.shape, dtype=np.float64)
+        dEs = np.zeros(Es.shape, dtype=np.float64)
+        dW = np.zeros(W.shape, dtype=np.float64)
+
+        # Place them in a list to ease further handling
+        derivatives = [dS, dA, dEs, dW]
+        species = [S, A, Es, W]
+
+        # Define stochiometry
+        stochiometry = [1, 1, -1, -1]
+
+        # Loop over species
+        for i in range(len(['S', 'A', 'Es', 'W'])):
+            # Loop over spatial axes
+            for j in range(1,S.shape[1]):
+                # Evaluate the enzyme kinetic model
+                v = 0
+                # Liquid phase
+                C = species[i]
+                # Intermediate nodes
+                if j < S.shape[1]-1:
+                    derivatives[i][0,j] = (D_ax/delta_x**2)*(C[0,j-1] - 2*C[0,j] + C[0,j+1]) - \
+                                          (u/delta_x)*(C[0,j] - C[0,j-1]) + \
+                                          (kL_a/epsilon)*(C[1,j] - C[0,j])
+                # Outlet boundary
+                elif j == S.shape[1]-1:
+                    derivatives[i][0,j] = (D_ax/delta_x**2)*(C[0,j-1] - 2*C[0,j] + C[0,j-1]) - \
+                                          (u/delta_x)*(C[0,j] - C[0,j-1]) + \
+                                          (kL_a/epsilon)*(C[1,j] - C[0,j])
+                # Solid phase
+                derivatives[i][1,j] = - (kL_a/epsilon)*(C[1,j] - C[0,j]) + rho_B*stochiometry[i]*v
+
+        # Unpack derivatives
+        dS, dA, dEs, dW = derivatives
+
+        return dS, dA, dEs, dW
+
+class PPBB_model(ODEModel):
+    """
+    A model for the 
+    S + A <--> Es + W
+    """
+    
+    state_names = ['S','A','Es','W']
+    parameter_names = ['c_enzyme', 'Vf_Ks', 'R_AS', 'R_AW', 'R_Es', 'K_eq', 'K_W', 'K_iEs']
+
+    @staticmethod
+    def integrate(t, S, A, Es, W, c_enzyme, Vf_Ks, R_AS, R_AW, R_Es, K_eq, K_W, K_iEs):
+
+        # Calculate rate
+        v = c_enzyme*(Vf_Ks*(S*A - (1/K_eq)*Es*W)/(A + R_AS*S + R_AW*W + R_AS*S*Es/K_iEs + R_Es*Es + R_Es*W*Es/K_W))
+       
+        return -v, -v, v, v
---
+++ tutorials/enzyme_kinetics/1D_PFR/simulation.py
@@ -0,0 +1,97 @@
+"""
+This script contains a calibration of a ping-pong bi-bi model to describe the enzymatic esterification reaction of D-Glucose and Lauric acid
+"""
+
+__author__      = "Tijs Alleman"
+__copyright__   = "Copyright (c) 2022 by T.W. Alleman, BIOMATH, Ghent University. All Rights Reserved."
+
+
+############################
+## Load required packages ##
+############################
+
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+
+################
+## Load model ##
+################
+
+from models import packed_PFR
+
+#################
+## Setup model ##
+#################
+
+# Design variables
+# ~~~~~~~~~~~~~~~~
+
+l = 1 # Reactor length (m)
+Q = (0.200/60)*10**-6   # Flow rate (m³/s)
+dt = 0.0024 # Tube inner diameter (m)
+dp = 0.0004755 # Catalyst particle diameter (m)
+mu = 3.35e-03 # Solvent dynamic viscosity (Pa.s)
+rho_B = 545 # Catalyst density (kg/m³)
+rho_F = 775 # Solvent density (kg/m³)
+
+# Derived variables
+# ~~~~~~~~~~~~~~~~~
+
+epsilon = 0.39 + 1.74/(dt/dp+1.140)**2 # Packed bed porosity (-)
+A = (dt/2)**2*math.pi # Reactor cross-sectional area (m2)
+U = Q/A
+u = U/epsilon # Fluid velocity (m/s)
+Re = dp*U*rho_F/mu    # Reynolds number (-)
+
+# Molar volumes
+L = 1;
+VaS = 0.32*L*(L-1)+9.74+4*21.95+12.79+31.57; #cm³/mol
+L = 12;
+VaA = 0.32*L*(L-1)+27.38+10*19.02+37.71; #cm³/mol
+L = 12+1;
+VaEs = 0.32*L*(L-1)+27.38+10*19.02+29.29+19.02+12.79+4*21.95+9.74; #cm³/mol
+# Diffusion constants in t-Butanol
+nB = 3.35e-03
+T = 273.15+50
+psiB = 1
+MWB = 74.12
+VA = np.array([VaS, VaA, VaEs, 18.75]) # molar volume (cm³/mol)
+VB = 0.32*3*(3-1)+3*27.38+18.01 
+D = (1e-4*(8.52*10**-8*T)/(nB*VB**(1/3))*(1.40*(VB/VA)**(1/3)+VB/VA))       
+# Compute axial dispersion and kL*a
+a = 6*(1-epsilon)/dp # Catalyst surface area (m-1)
+kL = np.mean((0.7*D + (dp*U)/(0.18+0.008*Re**0.59)))
+D_ax = np.mean(((1.09/100)*(D/dp)**(2/3)*(U)**(1/3)))
+
+# Simulation parameters
+N = 100 # spatial nodes
+
+# Define model parameters
+params={'epsilon': epsilon, 'kL_a': kL*a, 'D_ax': D_ax, 'delta_x': l/N, 'u': u, 'rho_B': rho_B, 'inlet': [30, 60, 18, 0]}
+
+# Define coordinates
+coordinates = {'phase': ['liquid','solid'], 'x': np.linspace(start=0, stop=l, num=N)}
+
+# Define initial condition
+S = A = np.zeros([len(coordinates['phase']), len(coordinates['x'])])
+S[:,0] = 30
+A[:,0] = 60
+W = 18*np.ones([len(coordinates['phase']), len(coordinates['x'])])
+init_states = {'S': S, 'A': A, 'W': W}
+
+# Initialize model
+model = packed_PFR(init_states, params, coordinates)
+
+####################
+## Simulate model ##
+####################
+
+out = model.sim(2000)
+
+fig,ax=plt.subplots()
+for t in [0, 100, 500, 1000, 2000]:
+    ax.plot(coordinates['x'], out['S'].sel(phase='liquid').isel(time=t), color='black')
+    ax.plot(coordinates['x'], out['S'].sel(phase='solid').isel(time=t), color='black', linestyle='--')
+plt.show()
+plt.close()
