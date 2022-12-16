47c8c5d5e5d7fedaa0acad3d5f700a327f870594
Tijs Alleman <tijs.alleman@ugent.be>
2022-12-16 18:15:15 +0100

    intrinsic enzyme concentration calibration

--- tutorials/enzyme_kinetics/calibrate_intrinsic_kinetics.py
+++ tutorials/enzyme_kinetics/calibrate_intrinsic_kinetics.py
@@ -81,30 +81,30 @@ if __name__ == '__main__':
     ## PSO/Nelder-Mead ##
     #####################
 
-    # Variables
-    processes = int(os.getenv('SLURM_CPUS_ON_NODE', mp.cpu_count()/2))
-    n_pso = 30
-    multiplier_pso = 10
     # Calibated parameters and bounds
     pars = ['Vf_Ks', 'R_AS', 'R_AW', 'R_Es', 'K_eq']
     labels = ['$V_f/K_S$','$R_{AS}$','$R_{AW}$','$R_{Es}$', '$K_{eq}$']
     bounds = [(1e-5,1e-2), (1e-2,10), (1e-2,10), (1e-2,10), (1e-2,2)]
     # Setup objective function (no priors --> uniform priors based on bounds)
-    objective_function = log_posterior_probability(model,pars,bounds,data,states,log_likelihood_fnc,log_likelihood_fnc_args,weights,initial_states=initial_concentrations,labels=labels)                               
+    objective_function = log_posterior_probability(model,pars,bounds,data,states,log_likelihood_fnc,log_likelihood_fnc_args,initial_states=initial_states,labels=labels)                               
+    # Settings
+    processes = int(os.getenv('SLURM_CPUS_ON_NODE', mp.cpu_count()/2))
+    n_pso = 30
+    multiplier_pso = 10   
     # PSO
     theta = pso.optimize(objective_function, swarmsize=multiplier_pso*processes, max_iter=n_pso, processes=processes, debug=True)[0]    
     # Nelder-mead
     step = len(theta)*[0.05,]
-    theta = nelder_mead.optimize(objective_function, np.array(theta), step, processes=processes, max_iter=n_pso)[0]
+    theta = nelder_mead.optimize(objective_function, theta, step, processes=processes, max_iter=n_pso)[0]
 
     ##########
     ## MCMC ##
     ##########
 
     # Variables
-    n_mcmc = 100
-    multiplier_mcmc = 9
-    print_n = 20
+    n_mcmc = 200
+    multiplier_mcmc = 18
+    print_n = 50
     discard = 50
     samples_path='sampler_output/'
     fig_path='sampler_output/'
@@ -112,20 +112,19 @@ if __name__ == '__main__':
     run_date = str(datetime.date.today())
     # Perturbate previously obtained estimate
     ndim, nwalkers, pos = perturbate_theta(theta, pert=0.10*np.ones(len(theta)), multiplier=multiplier_mcmc, bounds=bounds)
-    # Write some usefull settings to a pickle file (no pd.Timestamps or np.arrays allowed!)
+    # Write some usefull settings to a .json
     settings={'start_calibration': 0, 'end_calibration': 3000,
               'n_chains': nwalkers, 'starting_estimate': list(theta), 'labels': labels}
     # Sample 100 iterations
-    sampler = run_EnsembleSampler(pos, n_mcmc, identifier, objective_function, (), {},
+    sampler = run_EnsembleSampler(pos, n_mcmc, identifier, objective_function,
                                     fig_path=fig_path, samples_path=samples_path, print_n=print_n, backend=None, processes=processes, progress=True,
                                     settings_dict=settings)
     # Sample 100 more
     backend = emcee.backends.HDFBackend(os.path.join(os.getcwd(),samples_path+'username_BACKEND_'+run_date+'.hdf5'))
-    sampler = run_EnsembleSampler(pos, n_mcmc, identifier, objective_function, (), {},
+    sampler = run_EnsembleSampler(pos, n_mcmc, identifier, objective_function,
                                     fig_path=fig_path, samples_path=samples_path, print_n=print_n, backend=backend, processes=processes, progress=True,
                                     settings_dict=settings)
     # Generate a sample dictionary and save it as .json for long-term storage
-    # Have a look at the script `emcee_sampler_to_dictionary.py`, which does the same thing as the function below but can be used while your MCMC is running.
     samples_dict = emcee_sampler_to_dictionary(samples_path, identifier, discard=discard)
     # Look at the resulting distributions in a cornerplot
     CORNER_KWARGS = dict(smooth=0.90,title_fmt=".2E")
@@ -150,7 +149,7 @@ if __name__ == '__main__':
     # Loop over datasets
     for i,df in enumerate(data):
         # Update initial condition
-        model.initial_states.update(initial_concentrations[i])
+        model.initial_states.update(initial_states[i])
         # Simulate model
         out = model.sim(3000, N=N, draw_function=draw_fcn, samples=samples_dict)
         # Add 5% observational noise
@@ -164,6 +163,8 @@ if __name__ == '__main__':
         #ax.errorbar(df.index, df.values, yerr=log_likelihood_fnc_args[i], fmt='x', color='black')
         ax.legend(['data', 'D-glucose', 'Glucose laurate'])
         ax.grid(False)
+        ax.set_ylabel('species concentration (mM)')
+        ax.set_xlabel('time (min)')
         plt.tight_layout()
         plt.show()
         plt.close()
