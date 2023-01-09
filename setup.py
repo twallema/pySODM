633e830c7039ba464da40471fc53b85964eb3197
Tijs Alleman <tijs.alleman@ugent.be>
2023-01-09 11:37:07 +0100

    version 0.1.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='pySODM',
     packages=find_packages("src", exclude=["*.tests"]),
     package_dir={'': 'src'},
-    version='0.1',
+    version='0.1.1',
     description='Simulating and Optimising Dynamical Models',
     author='Tijs Alleman, KERMIT, Ghent University',
     license='MIT',
