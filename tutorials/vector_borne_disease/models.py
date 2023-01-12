d251ed8dc1697230061abf53c668ed1776032871
Tijs Alleman <tijs.alleman@ugent.be>
2023-01-12 19:14:48 +0100

    vector example

--- tutorials/vector_borne_disease/models.py
+++ tutorials/vector_borne_disease/models.py
@@ -7,22 +7,23 @@ class SIR_SI(ODEModel):
     """
 
     state_names = ['S', 'I', 'R', 'S_v', 'I_v']
-    parameter_names = ['gamma']
+    parameter_names = ['beta', 'gamma']
     parameter_stratified_names = ['alpha']
     stratification_names = ['age_group']
     state_stratifications = [['age_group'],['age_group'],['age_group'],[],[]]
 
     @staticmethod
-    def integrate(t, S, I, R, S_v, I_v, alpha, gamma):
+    def integrate(t, S, I, R, S_v, I_v, alpha, beta, gamma):
 
         # Calculate total mosquito population
+        N = S + I + R
         N_v = S_v + I_v
         # Calculate human differentials
         dS = -alpha*(I_v/N_v)*S
         dI = alpha*(I_v/N_v)*S - 1/gamma*I
         dR = 1/gamma*I
         # Calculate mosquito differentials
-        dS_v = -np.sum(alpha*(I_v/N_v)*S_v)
-        dI_v = np.sum(alpha*(I_v/N_v)*S_v)
+        dS_v = -np.sum(alpha*(I/N)*S_v) + (1/beta)*N_v - (1/beta)*S_v
+        dI_v = np.sum(alpha*(I/N)*S_v) - (1/beta)*I_v
 
         return dS, dI, dR, dS_v, dI_v
--- tutorials/vector_borne_disease/test.py
+++ tutorials/vector_borne_disease/test.py
@@ -26,7 +26,7 @@ warnings.filterwarnings("ignore")
 from models import SIR_SI
 
 # Define parameters and initial condition
-params={'alpha': [0.01, 0.01, 0.02, 0.01], 'gamma': 5}
+params={'alpha': 10*np.array([0.005, 0.01, 0.02, 0.015]), 'gamma': 5, 'beta': 5}
 init_states = {'S': [606938, 1328733, 7352492, 2204478], 'S_v': 1e6, 'I_v': 1}
 # Define model coordinates
 age_groups = pd.IntervalIndex.from_tuples([(0,5),(5,15),(15,65),(65,120)])
@@ -36,13 +36,23 @@ coordinates={'age_group': age_groups}
 model = SIR_SI(states=init_states, parameters=params, coordinates=coordinates)
 
 # Simulate model
-out = model.sim(121)
+out = model.sim(365)
+print(out)
 
 # Visualize result
-fig,ax=plt.subplots(figsize=(8,6))
-ax.plot(out['time'], out['S_v'], color='green')
-ax.plot(out['time'], out['I_v'], color='red')
-for age_group in age_groups:
-    ax.plot(out['time'], out['I'].sel(age_group=age_group), color='black')
+fig,ax=plt.subplots(nrows=2, ncols=1, figsize=(8,6), sharex=True)
+ax[0].plot(out['time'], out['S_v']/init_states['S_v']*100, color='green', label='Susceptible')
+ax[0].plot(out['time'], out['I_v']/init_states['S_v']*100, color='red', label='Infected')
+ax[0].set_ylabel('Health state vectors (%)')
+ax[0].legend()
+ax[0].set_title('Vector lifespan: ' + str(params['beta']) + ' days')
+colors=['black', 'red', 'green', 'blue']
+labels=['0-5','5-15','15-65','65-120']
+for i,age_group in enumerate(age_groups):
+    ax[1].plot(out['time'], out['I'].sel(age_group=age_group)/init_states['S'][i]*100000, color=colors[i], label=labels[i])
+ax[1].set_ylabel('Infectious humans per 100K')
+ax[1].legend()
+ax[1].set_xlabel('time (days)')
+ax[1].set_title('Vector-to-human transfer rate: '+str(params['alpha']))
 plt.show()
 plt.close()
