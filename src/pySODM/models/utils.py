5a1cd4bb28dea2f529368d47252ab54d11dca325
Tijs Alleman <tallema2@jh.edu>
2024-08-30 16:38:18 -0400

    change the behavior of retain_floats to False because it does not wor… (#84)

--- src/pySODM/models/utils.py
+++ src/pySODM/models/utils.py
@@ -5,10 +5,10 @@ def int_to_date(actual_start_date, t):
     date = actual_start_date + timedelta(days=t)
     return date
 
-def list_to_dict(y, shape_dictionary, retain_floats=True):
+def list_to_dict(y, shape_dictionary, retain_floats=False):
     """
     A function to reconstruct model states, given a flat array of values and a dictionary with the variables name and desired shapes
-    Retains floats.
+    Does not retains floats.
 
     Parameters
     ----------
@@ -20,7 +20,8 @@ def list_to_dict(y, shape_dictionary, retain_floats=True):
         A dictionary containing the desired names and shapes of the output dictionary
     
     retain_floats: bool
-        If False, float/np.float are converted in a 0D np.ndarray. Default: True.
+        If False, states initially given to pySODM as a float are converted in a 0D np.ndarray before being handed off to the integration function.
+        This results in an apparent 'change' in datatype of model states to the user of an unstratified model. Computationally there are no consequences. Default: False.
 
     Returns
     -------
