5096144bfe39a05bd91dd988cf15e9384a5a4f42
Tijs Alleman <tallema2@jh.edu>
2025-01-13 18:57:13 +0100

    Function to cut out ICF params (#120)

--- src/pySODM/models/utils.py
+++ src/pySODM/models/utils.py
@@ -41,3 +41,37 @@ def list_to_dict(y, shape_dictionary, retain_floats=False):
             restoredArray.append(y[offset:(offset+n)].reshape(s))
         offset+=n
     return dict(zip(shape_dictionary.keys(), restoredArray))
+
+
+def cut_ICF_parameters_from_parameters(parameters_names_modeldeclaration, extra_params_TDPF, extra_params_ICF, parameters):
+    """
+    A function to cut the initial condition function's parameters from the model parameters dictionary
+
+    input
+    -----
+
+    parameters_names_modeldeclaration: list
+        Parameter names provided by the user in the model declaration
+    
+    extra_params_TDPF: list
+        Parameter names of the time-dependent parameter functions
+    
+    extra_params_ICF: list
+        Parameter names of the initial condition function
+
+    parameters: dict
+        Model parameters dictionary
+
+    output
+    ------
+
+    parameters: dict
+        Model parameters dictionary, minus the parameters unique to the initial condition function
+    """
+
+    # Compute union of TDPF parameters and parameters in model declaration
+    union_TDPF_integrate = set(extra_params_TDPF) | set(parameters_names_modeldeclaration)
+    # Compute the difference between initial condition pars and union TDPF + model declaration pars to obtain unique ICF pars
+    unique_ICF = set(extra_params_ICF) - union_TDPF_integrate 
+    # Retain all but unique ICF pars
+    return {key: value for key, value in parameters.items() if key in union_TDPF_integrate or key not in unique_ICF}
